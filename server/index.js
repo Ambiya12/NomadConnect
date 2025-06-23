7780c0259739c4cea3414068a8ec2ae2361c28d4
Ambiya12 <ambiyadms@gmail.com>
2025-06-23 16:20:05 +0200

    feat: integrate authentication router into the server

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors'
 import connectDB from './database/client.js'
+import authRouter from './routes/auth.js';
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-    
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/api', authRouter);
 
 connectDB();
 app.listen(8000, () => {
