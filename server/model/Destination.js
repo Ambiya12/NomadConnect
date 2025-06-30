eecbc7a75c49a49d5627867d6f7d5001e610669d
Ambiya12 <ambiyadms@gmail.com>
2025-06-30 10:31:04 +0200

    feat: add Destination model with schema for managing travel destinations

---
+++ server/model/Destination.js
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from "mongoose";
+
+const destinationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  images: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"], 
+    },
+    coordinates: {
+      type: [Number],
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  tags: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+export default mongoose.model("Destination", destinationSchema);
