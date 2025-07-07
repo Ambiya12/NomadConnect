32a79df13020e624254a4c2262fb304324162fc0
Ambiya12 <ambiyadms@gmail.com>
2025-07-07 02:32:49 +0200

    feat: new schema travel tip

---
+++ server/model/TravelTip.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const travelTipSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("TravelTip", travelTipSchema);
